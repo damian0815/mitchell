6aa1a5c75b110bc4eee4e3ef8f5d38904b685c46
damian <git@damianstewart.com>
2017-10-19 20:49:27 +0200

    mitchell can walk around the bush

--- game.js
+++ game.js
@@ -1,18 +1,57 @@
 function gameFunction() { 
 
-        var g_game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+	var g_game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-		var g_mitchell;
-		var g_state;
 
-        function preload () {
-			console.log('preload');
-			Mitchell.preload(g_game);
-        }
+	var g_mitchell;
+	var g_state;
 
-        function create () {
-			console.log('create');
-			g_mitchell = new Mitchell(g_game);
+	function preload () {
+		console.log('preload');
+		Mitchell.preload(g_game);
+	}
 
-        }
+	function create () {
+		console.log('create');
+		g_mitchell = new Mitchell(g_game);
+		
+		g_game.stage.backgroundColor = '#283811';
+
+	}
+
+	function update() {
+	
+	/*
+    if (g_game.input.activePointer.withinGame)
+    {
+        g_game.input.enabled = true;
+        g_game.stage.backgroundColor = '#736357';
+    }
+    else
+    {
+        g_game.input.enabled = false;
+        g_game.stage.backgroundColor = '#731111';
+    }*/
+
+	
+		if (g_game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+		{
+			g_mitchell.move(-4,0);
+		}
+		else if (g_game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+		{
+			g_mitchell.move(4,0);
+		}
+
+		if (g_game.input.keyboard.isDown(Phaser.Keyboard.UP))
+		{
+			g_mitchell.move(0, -4);
+		}
+		else if (g_game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+		{
+			g_mitchell.move(0, 4);
+		}
+
+	}
+
 }
--- mitchell.js
+++ mitchell.js
@@ -1,7 +1,13 @@
 
 class Mitchell {
+
 	constructor(game) {
 		this.m_sprite = game.add.sprite(0,0,'mitchell');
+	}
+
+	move(x,y) {
+		this.m_sprite.x += x;
+		this.m_sprite.y += y;
 	}
 
 };
